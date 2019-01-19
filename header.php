f0c16fda94268b28eb949a3334f4d6eba69177b4
Loic NOEL <loicnoel12@gmail.com>
2019-01-19 12:11:45 +0100

    Add a some cosmetic improvments

--- header.php
+++ header.php
@@ -15,22 +15,22 @@
           <a class="navbar-brand" href="<?php echo get_site_url() ?>">
             <img id="logo_mf" src="<?php echo get_template_directory_uri() . '/logo_mf.svg'; ?>">
           </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-
-          <?php
-            wp_nav_menu(
-              array(
-                'container'       => 'div',
-                'container_class' => 'collapse navbar-collapse',
-                'menu_class'      => 'menu',
-                'echo'            => true,
-                'fallback_cb'     => 'wp_page_menu',
-                'items_wrap'      => '<ul class="navbar-nav ml-auto">%3$s</ul>',
-                'depth'           => 0,
-                'walker'          => new Mf_theme_nav_menu_walker
-              )
-            );
-          ?>
+            <?php
+              wp_nav_menu(
+                array(
+                  'container'       => 'div',
+                  'container_class' => 'collapse navbar-collapse',
+                  'container_id'    => 'navbarMenu',
+                  'menu_class'      => 'menu',
+                  'echo'            => true,
+                  'fallback_cb'     => 'wp_page_menu',
+                  'items_wrap'      => '<ul class="navbar-nav ml-auto">%3$s</ul>',
+                  'depth'           => 0,
+                  'walker'          => new Mf_theme_nav_menu_walker
+                )
+              );
+            ?>
         </nav>
--- page-about-me.php
+++ page-about-me.php
@@ -14,16 +14,17 @@
   <div class="starter-template">
 
     <div class="row">
-      <div class="col-7 about-me-image">
+      <div class="col-xs-12 col-md-7">
+        <div class="about-me-image">
+        </div>
       </div>
       <?php
           if (have_posts()) {
             while (have_posts()) {
               the_post();
         ?>
-          <div class="col-5 about-me">
+          <div class="col-xs-12 col-md-5 about-me">
             <div class="about-me-content">
-              <h1><?php the_title(); ?></h1>
               <p><?php the_content(); ?></p>
             </div>
           </div>
