21323ed45d42174d6a5b8b1f79595b9252fdaf6e
Loic NOEL <loicnoel12@gmail.com>
2019-04-18 00:03:51 +0200

    Add home page template

---
+++ page-home.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+  Template Name: Home page
+ */
+?>
+
+<!DOCTYPE html>
+
+<html lang="fr">
+        <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <title>Mathilde Faivre Website</title>
+                <link href="https://fonts.googleapis.com/css?family=Cardo" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400" rel="stylesheet">
+                <?php wp_head(); ?>
+        </head>
+      <body class="home-page-body">
+
+        <nav class="navbar navbar-dark fixed-top">
+          <a class="navbar-brand" href="">
+          </a>
+          <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+            <?php
+              wp_nav_menu(
+                array(
+                  'container'       => 'div',
+                  'container_class' => 'collapse navbar-collapse',
+                  'container_id'    => 'navbarMenu',
+                  'menu_class'      => 'menu',
+                  'echo'            => true,
+                  'fallback_cb'     => 'wp_page_menu',
+                  'items_wrap'      => '<ul class="navbar-nav ml-auto">%3$s</ul>',
+                  'depth'           => 0,
+                  'walker'          => new Mf_theme_nav_menu_walker
+                )
+              );
+            ?>
+        </nav>
+
+<main role="main" class="container">
+
+  <div class="starter-template">
+
+    <div class="row home-page">
+      <?php
+          if (have_posts()) {
+            while (have_posts()) {
+              the_post();
+        ?>
+              <div class="col-9">
+                <p><?php the_content(); ?></p>
+              </div>
+
+        <?php
+            }
+          }
+        ?>
+      </div>
+
+  </div>
+
+</main>
+
+<?php get_footer(); ?>
