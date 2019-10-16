5cb462a4f9115851913ef5ef15c4df6997ce0697
Loic NOEL <loicnoel12@gmail.com>
2019-10-16 12:52:52 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+* The template for displaying 404 pages (Not Found)
+ */
+
+get_header();
+?>
+
+
+<main role="main" class="container-fluid">
+
+  <div class="starter-template">
+			<div class="container" style="margin-top:100px;min-height:100px;">
+						<h2>C'est embarassant...</h2>
+						<p>
+							Il semblerait qu'aucune page n'existe à cet endroit.
+						</p>
+			</div>
+  </div>
+
+  </div>
+
+</main>
+
+<?php get_footer(); ?>
